8d080fb5546d42d130331efe0c085d54db5bf01f
Carson Holloway <carsonalh@gmail.com>
2021-08-22 16:35:00 +1000

    Created very bare bones WebGL functionality.

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.bundle.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template: './src/index.html'})
+    ]
+};

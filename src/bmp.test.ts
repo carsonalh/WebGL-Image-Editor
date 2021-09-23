1c169e78ca4c806385c73b9535e24aef79b59d27
Carson Holloway <carsonalh@gmail.com>
2021-09-23 22:05:35 +1000

    Use BITMAPINFOHEADER and write image to bmp
    The `Bmp` class could only write an image filled with black of size n x
    m. Now, it can write an image filled with arbitrary image data, as
    specified by the red, green, and blue channels given by the user.
    
    Also note that the use of BITMAPINFOHEADER is hard-coded, which may need
    to change in the future.
    
    The API in its current state is far from decent and will need a rework
    in the near future, but what is here should work as a proof of concept.

--- src/bmp.test.ts
+++ src/bmp.test.ts
@@ -6,7 +6,8 @@ describe('bmp with default options', () => {
     const byteArray = new Uint8Array(buffer);
 
     it('creates an array large enough for the file header and the DIB header', () => {
-        expect(buffer.byteLength).toBeGreaterThanOrEqual(0x20);
+        // NOTE: using BITMAPINFOHEADER as header standard
+        expect(buffer.byteLength).toBeGreaterThanOrEqual(0x36);
     });
 
     it('writes BM to the start of the file', () => {
@@ -15,17 +16,17 @@ describe('bmp with default options', () => {
         expect(byteArray[1]).toEqual(0x4d);
     });
 
-    it('writes decimal 12 to the first 4 bytes of the DIB header (starting at 0x0E)', () => {
-        // Little endian is king
-        expect(byteArray[0x0e]).toEqual(12);
-        expect(byteArray[0x0f]).toEqual(0);
-        expect(byteArray[0x10]).toEqual(0);
-        expect(byteArray[0x11]).toEqual(0);
+    it('writes decimal 40 to the first 4 bytes of the DIB header (starting at 0x0E)', () => {
+        // decimal 40 is 0x28
+        expect(byteArray[0x0e]).toEqual(0x28);
+        expect(byteArray[0x0f]).toEqual(0x00);
+        expect(byteArray[0x10]).toEqual(0x00);
+        expect(byteArray[0x11]).toEqual(0x00);
     });
 
-    it('writes 1 as the number of color planes at offset 0x16', () => {
-        expect(byteArray[0x16]).toEqual(0x01);
-        expect(byteArray[0x17]).toEqual(0x00);
+    it('writes 1 as the number of color planes at offset 0x1A', () => {
+        expect(byteArray[0x1a]).toEqual(0x01);
+        expect(byteArray[0x1b]).toEqual(0x00);
     });
 
     it('writes 0 as the width and height in the DIB header', () => {
@@ -38,23 +39,31 @@ describe('bmp with default options', () => {
     });
 
     it('uses 24 bits per pixel by default', () => {
-        expect(byteArray[0x18]).toEqual(0x18); // 24 in decimal
-        expect(byteArray[0x19]).toEqual(0x00);
+        expect(byteArray[0x1c]).toEqual(0x18); // 24 in decimal
+        expect(byteArray[0x1d]).toEqual(0x00);
     });
 
     it('stores no data for an image with zero width and zero height', () => {
-        expect(byteArray.length).toEqual(0x20);
+        expect(byteArray.length).toEqual(0x36);
     });
 
     it('stores the size of the whole file in the file header', () => {
-        expect(byteArray[0x02]).toEqual(0x20);
+        expect(byteArray[0x02]).toEqual(0x36);
         expect(byteArray[0x03]).toEqual(0x00);
         expect(byteArray[0x04]).toEqual(0x00);
         expect(byteArray[0x05]).toEqual(0x00);
     });
+
+    it('should use BI_RGB compression by default', () => {
+        // BI_RGB is represented as 0
+        expect(byteArray[0x1e]).toEqual(0x00);
+        expect(byteArray[0x1f]).toEqual(0x00);
+        expect(byteArray[0x20]).toEqual(0x00);
+        expect(byteArray[0x21]).toEqual(0x00);
+    });
 });
 
-describe('bmp image of size 2x2', () => {
+describe('bmp image of size 2 x 2', () => {
     const bmp = Bmp.create({
         width: 2,
         height: 2,
@@ -64,13 +73,87 @@ describe('bmp image of size 2x2', () => {
 
     it('pads 6 bytes of color data to 8 (4 byte alignment per row)', () => {
         // There should be 16 bytes of image data, as there are 2 rows
-        expect(byteArray[0x02]).toEqual(0x30);
+        expect(byteArray[0x02]).toEqual(0x46);
         expect(byteArray[0x03]).toEqual(0x00);
         expect(byteArray[0x04]).toEqual(0x00);
         expect(byteArray[0x05]).toEqual(0x00);
     });
 
-    it('creates buffer of size 0x30 (0x20 header + 0x10 in image data)', () => {
-        expect(buffer.byteLength).toEqual(0x30);
+    it('creates buffer of size 0x46 (0x36 header + 0x10 in image data)', () => {
+        expect(buffer.byteLength).toEqual(0x46);
+    });
+});
+
+describe('bmp image of size 0x100 x 0x100', () => {
+    const bmp = Bmp.create({
+        width: 0x100,
+        height: 0x100,
+    });
+    const buffer = bmp.toBuffer();
+    const byteArray = new Uint8Array(buffer);
+
+    it('can store the width and height in the DIB header', () => {
+        // width
+        expect(byteArray[0x12]).toEqual(0x00);
+        expect(byteArray[0x13]).toEqual(0x01);
+        expect(byteArray[0x14]).toEqual(0x00);
+        expect(byteArray[0x15]).toEqual(0x00);
+        // height
+        expect(byteArray[0x16]).toEqual(0x00);
+        expect(byteArray[0x17]).toEqual(0x01);
+        expect(byteArray[0x18]).toEqual(0x00);
+        expect(byteArray[0x19]).toEqual(0x00);
+    });
+
+    it('should have a size of exactly 0x36 + 3 * (0x100)^2 as 0x100 is divisible by 4', () => {
+        const bytesPerRow = 3 * 0x100;
+        expect(buffer.byteLength).toEqual(0x36 + bytesPerRow * 0x100);
+    });
+
+    it('should write the exact size in metadata (location 0x02 for 4 bytes)', () => {
+        // Expecting the 4-byte value (0x)00 03 00 36
+        expect(byteArray[0x02]).toEqual(0x36);
+        expect(byteArray[0x03]).toEqual(0x00);
+        expect(byteArray[0x04]).toEqual(0x03);
+        expect(byteArray[0x05]).toEqual(0x00);
+    });
+});
+
+describe('2x2 image from wikipedia example https://en.wikipedia.org/wiki/BMP_file_format#Example_1', () => {
+    const bmp = Bmp.create({
+        width: 2,
+        height: 2,
+        redChannel: new Uint8Array([0xff, 0xff, 0x00, 0x00]),
+        blueChannel: new Uint8Array([0x00, 0xff, 0xff, 0x00]),
+        greenChannel: new Uint8Array([0x00, 0xff, 0x00, 0xff]),
+    });
+    const buffer = bmp.toBuffer();
+    const byteArray = new Uint8Array(buffer);
+
+    it('can write the image from wikipedia to bmp', () => {
+        // Should be written as BGR
+
+        // (0, 1)
+        expect(byteArray[0x36]).toEqual(0x00);
+        expect(byteArray[0x37]).toEqual(0x00);
+        expect(byteArray[0x38]).toEqual(0xff);
+        // (1, 1)
+        expect(byteArray[0x39]).toEqual(0xff);
+        expect(byteArray[0x3a]).toEqual(0xff);
+        expect(byteArray[0x3b]).toEqual(0xff);
+        // Padding for row y = 1
+        expect(byteArray[0x3c]).toEqual(0x00);
+        expect(byteArray[0x3d]).toEqual(0x00);
+        // (0, 0)
+        expect(byteArray[0x3e]).toEqual(0xff);
+        expect(byteArray[0x3f]).toEqual(0x00);
+        expect(byteArray[0x40]).toEqual(0x00);
+        // (1, 0)
+        expect(byteArray[0x41]).toEqual(0x00);
+        expect(byteArray[0x42]).toEqual(0xff);
+        expect(byteArray[0x43]).toEqual(0x00);
+        // Padding for row y = 0
+        expect(byteArray[0x44]).toEqual(0x00);
+        expect(byteArray[0x45]).toEqual(0x00);
     });
 });
--- src/bmp.ts
+++ src/bmp.ts
@@ -3,23 +3,74 @@ export interface BmpOptions {
     width?: number;
     /** The height of the image (default 0) */
     height?: number;
+    /** The red channel; row major; y-positive; must be length width * height or greater */
+    redChannel?: Uint8Array;
+    /** The blue channel; row major; y-positive; must be length width * height or greater */
+    blueChannel?: Uint8Array;
+    /** The green channel; row major; y-positive; must be length width * height or greater */
+    greenChannel?: Uint8Array;
 }
 
 /**
  * Manages converting image data to the BMP file format.
  */
 class Bmp {
-    static create(options?: BmpOptions) {
+    public static create(options?: BmpOptions) {
         return new Bmp(options);
+    }
+
+    /**
+     * Writes an amount of bytes from a given number value to a Uint8Array.
+     *
+     * Note that if a number that does not fit into the amount of bytes is
+     * provided, it will be cut off at the given amount of bytes. Also note that
+     * this has NO SUPPORT for negative numbers.
+     *
+     * @param byteArray The array of bytes to which to write
+     * @param value     The number value to be written
+     * @param offset    The exact byte at which to start writing
+     * @param numBytes  The number of bytes to write to the array
+     */
+    private static writeLittleEndianBytes(
+        byteArray: Uint8Array,
+        value: number,
+        offset: number,
+        numBytes: number
+    ) {
+        for (let i = offset; i < offset + numBytes; i++) {
+            byteArray[i] = value & 0xff;
+            value >>= 8;
+        }
     }
 
     private _width: number;
     private _height: number;
+    private _redChannel: Uint8Array;
+    private _blueChannel: Uint8Array;
+    private _greenChannel: Uint8Array;
+
+    /** The width of the image, in pixels */
+    public get width() {
+        return this._width;
+    }
+
+    /** The height of the image, in pixels */
+    public get height() {
+        return this._height;
+    }
 
     private constructor(options?: BmpOptions) {
+        const numPixels =
+            options?.width !== undefined && options?.height !== undefined
+                ? options.width * options.height
+                : 0;
+
         const defaultOptions = <BmpOptions>{
             width: 0,
             height: 0,
+            redChannel: new Uint8Array(numPixels),
+            greenChannel: new Uint8Array(numPixels),
+            blueChannel: new Uint8Array(numPixels),
         };
 
         // Create a copy of the options with a default for options that are not
@@ -29,6 +80,9 @@ class Bmp {
 
         this._width = _options.width;
         this._height = _options.height;
+        this._redChannel = _options.redChannel;
+        this._greenChannel = _options.greenChannel;
+        this._blueChannel = _options.blueChannel;
     }
 
     /**
@@ -37,36 +91,49 @@ class Bmp {
      */
     public toBuffer() {
         const imageDataSizeInBytes = this.getImageDataSizeInBytes();
-        const buffer = new ArrayBuffer(0x20 + imageDataSizeInBytes);
+        const buffer = new ArrayBuffer(0x36 + imageDataSizeInBytes);
         const byteArray = new Uint8Array(buffer);
+
         // WRITE THE FILE HEADER
         // Write BM to the first 2 bytes of the file header
         byteArray[0x00] = 0x42;
         byteArray[0x01] = 0x4d;
         // Write the size of the entire file to the file header
-        byteArray[0x02] = 0x20 + imageDataSizeInBytes; // TODO: Assuming 0x00 <= imageDataSizeInBytes <= 0xDF
-        byteArray[0x03] = 0x00;
-        byteArray[0x04] = 0x00;
-        byteArray[0x05] = 0x00;
+        Bmp.writeLittleEndianBytes(byteArray, buffer.byteLength, 0x02, 4);
+
         // WRITE THE DIB HEADER
-        // Write 12 decimal (0C hex) (the size of the dib header) to the dib header
-        byteArray[0x0e] = 0x0c;
-        byteArray[0x0f] = 0x00;
-        byteArray[0x10] = 0x00;
-        byteArray[0x11] = 0x00;
-        // Write the width of the image data (16-bit unsigned)
-        byteArray[0x12] = this._width; // TODO: Assuming 0 <= this._width <= 0xFF
-        byteArray[0x13] = 0x00;
-        // Write the height of the image data (16-bit unsigned)
-        byteArray[0x14] = this._height; // TODO: Assuming 0 <= this._height <= 0xFF
-        byteArray[0x15] = 0x00;
+        // Write 40 decimal (0x28) (the size of the dib header) to the start of the dib header
+        Bmp.writeLittleEndianBytes(byteArray, 0x28, 0x0e, 4);
+        // Write the width and height to 0x12 and 0x14 of the image data (16-bit unsigned, each)
+        Bmp.writeLittleEndianBytes(byteArray, this._width, 0x12, 4);
+        Bmp.writeLittleEndianBytes(byteArray, this._height, 0x16, 4);
         // Write the number of color planes (16-bit unsigned; must be 1)
-        byteArray[0x16] = 0x01;
-        byteArray[0x17] = 0x00;
-        // Write the number of bits per pixel (16-bit unsigned)
-        // 24 bits per pixel
-        byteArray[0x18] = 0x18;
-        byteArray[0x19] = 0x00;
+        Bmp.writeLittleEndianBytes(byteArray, 1, 0x1a, 2);
+        // Write the number of bits per pixel (24bpp) (16-bit unsigned)
+        Bmp.writeLittleEndianBytes(byteArray, 24, 0x1c, 2);
+
+        const bytesPerRow = this.getNumBytesPerRow();
+        const HEADER_SIZE = 0x36;
+
+        // Write all the image data to the image
+        for (let y = 0; y < this.height; y++) {
+            // Writing the pixels as 24-bit BGR
+
+            // Note that since all the bytes are zero by default, all the
+            // padding should work itself out
+
+            for (let x = 0; x < this.width; x++) {
+                // TODO: make this at least _a little_ more intelligible
+                const px = bytesPerRow * y + 3 * x;
+                const blue = this._blueChannel[y * this._width + x];
+                const green = this._greenChannel[y * this._width + x];
+                const red = this._redChannel[y * this._width + x];
+                byteArray[HEADER_SIZE + px + 0] = blue;
+                byteArray[HEADER_SIZE + px + 1] = green;
+                byteArray[HEADER_SIZE + px + 2] = red;
+            }
+        }
+
         return buffer;
     }
 
@@ -75,11 +142,19 @@ class Bmp {
      * BMP file format.
      */
     private getImageDataSizeInBytes() {
+        const numTotalBytes = this.getNumBytesPerRow() * this._height;
+        return numTotalBytes;
+    }
+
+    /**
+     * Gets the number of bytes a single row in the image occupies, including
+     * its padding.
+     */
+    private getNumBytesPerRow() {
         const bytesPerPixel = 3; // 24-bit color
         const numUnpaddedBytesPerRow = bytesPerPixel * this._width;
         const numPaddedBytesPerRow = 4 * Math.ceil(numUnpaddedBytesPerRow / 4);
-        const numTotalBytes = numPaddedBytesPerRow * this._height;
-        return numTotalBytes;
+        return numPaddedBytesPerRow;
     }
 }
 

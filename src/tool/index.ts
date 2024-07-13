da19f361d709f94e7b4a135e7d15e068ee646e5f
Carson Holloway <carsonalh@gmail.com>
2024-07-13 12:48:28 +1000

    Fix import not at beginning for tool/index

--- src/tool/index.ts
+++ src/tool/index.ts
@@ -1,11 +1,11 @@
+import DotTool from './dot-tool';
+import LineTool from './line-tool';
+
 export type ToolKey = 'dot' | 'line';
 
 export interface Tool {
     onMouseDown(imageX: number, imageY: number): void;
     onMouseUp(imageX: number, imageY: number): void;
 };
-
-import DotTool from './dot-tool';
-import LineTool from './line-tool';
 
 export { DotTool, LineTool };
